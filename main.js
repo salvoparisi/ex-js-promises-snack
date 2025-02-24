646e3be0c36ea32e57af7f4fa198e3cf1248227f
salvoparisi <psalvo065@gmail.com>
2025-02-24 11:04:29 +0100

    add snack 2

--- main.js
+++ main.js
@@ -11,4 +11,23 @@ function getPostTitle(id) {
 
 console.log(getPostTitle(1));
 
+/* SNACK 2 */
+function lanciaDado() {
+    const promise = new Promise((resolve, reject) => {
+        console.log("lanciando il dado...");
+        const numeroCasuale = Math.floor(Math.random() * 5) + 1
+        setTimeout(() => {
+            if (numeroCasuale === 1) {
+                reject("Il dado si è incastrato")
+            } else {
+                const numeroDado = Math.floor(Math.random() * 6) + 1
+                resolve(numeroDado)
+                console.log(numeroDado);
+            }
+        }, 3000)
+    })
+    return promise
+}
+
+lanciaDado()
 
